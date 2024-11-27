0fdae8a486f447a1e9caff248b62a1dfd7b02b6c
juandi247 <jdiaz873@unab.edu.co>
2024-11-27 13:54:26 -0500

    prueba4

--- src/main/java/com/prueba/juandiego/config/CorsConfig.java
+++ src/main/java/com/prueba/juandiego/config/CorsConfig.java
@@ -3,21 +3,44 @@ package com.prueba.juandiego.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
+
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3001")); // Origen de tu cliente
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // Cabeceras permitidas
+        corsConfig.setAllowCredentials(true); // Permitir credenciales (cookies o tokens)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig); // Habilitar CORS para todas las rutas
+
+        return source;
+    }
+
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Permitir todos los endpoints
-                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins("*") // Permitir todos los orígenes
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                         .allowedHeaders("*") // Permitir todos los encabezados
-                        .allowCredentials(true); // No se necesitan credenciales si permites todo
+                        .allowCredentials(false); // No se necesitan credenciales si permites todo
             }
         };
     }
