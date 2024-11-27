b27c6deded5eaa719f07d15361d2528e5cc9588b
juandi247 <jdiaz873@unab.edu.co>
2024-11-27 08:56:28 -0500

    agregar el cors

---
+++ src/main/java/com/prueba/juandiego/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.prueba.juandiego.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permitir todos los endpoints
+                        .allowedOrigins("*") // Permitir todos los orígenes
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permitir todos los encabezados
+                        .allowCredentials(false); // No se necesitan credenciales si permites todo
+            }
+        };
+    }
+
+}
